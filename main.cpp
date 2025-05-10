80f88a4d5e41dcd70d1d9d93a90183aed9f0b608
Lance J <135398581+LanceJ01@users.noreply.github.com>
2025-05-10 13:31:11 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstddef>
+
+class BumpAllocator {
+private:
+    char *buffer;
+    size_t capacity;
+    size_t offset;
+
+public:
+    char *allocate(size_t size) {
+
+        if (!((offset + size) <= capacity)) {
+            return nullptr;
+        } else {
+            char *ptr = buffer + offset;
+            offset += size;
+            return ptr;
+        }
+    }
+};
+
+int main() {
+    return 0;
+}
